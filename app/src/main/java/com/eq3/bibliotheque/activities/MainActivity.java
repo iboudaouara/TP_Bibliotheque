2296d8ec2735d6a2df797ccd0fa323d28272de5f
Melissa <hmmelissa64@gmail.com>
2024-07-26 10:44:30 -0400

    package activities

--- app/src/main/java/com/eq3/bibliotheque/activities/MainActivity.java
+++ app/src/main/java/com/eq3/bibliotheque/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.eq3.bibliotheque;
+package com.eq3.bibliotheque.activities;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.eq3.bibliotheque.R;
 
 public class MainActivity extends AppCompatActivity {
 
