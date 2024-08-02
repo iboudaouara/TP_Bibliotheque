1e4d031bc738cb46a6e8bcdf91eaeebcce2322eb
Alycia51 <158513905+Alycia51@users.noreply.github.com>
2024-08-02 10:34:07 -0400

    connexion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation ("com.squareup.okhttp3:okhttp:4.9.1")
+    implementation ("com.fasterxml.jackson.core:jackson-databind:2.12.3")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
